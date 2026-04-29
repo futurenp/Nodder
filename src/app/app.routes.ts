a4453fd6857226415f78e7208b7b855e7227c9ff
futurenp <nathantylicki@gmail.com>
2026-04-29 17:50:54 -0500

    Main page & page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const m = await import('./home/home');
+      return m.Home;
+    }
+  },
+  {
+    path: 'statistics',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const m = await import('./statistics/statistics');
+      return m.Statistics;
+    }
+  },
+  {
+    path: 'publish',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const m = await import('./publish/publish');
+      return m.Publish;
+    }
+  },
+  {
+    path: 'user',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const m = await import('./user/user');
+      return m.User;
+    }
+  }
+];
---
+++ src/app/publish/publish.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publish',
+  imports: [],
+  templateUrl: './publish.html',
+  styleUrl: './publish.css',
+})
+export class Publish {}
